162e8509aa2293a7f2903c24a63aa1284a9d61fd
hexap <47399791+closedAi@users.noreply.github.com>
2021-03-23 22:49:48 +0300

    Update main.c
    Made the code more scalable so other DFA can be added

--- main.c
+++ main.c
@@ -1,51 +1,72 @@
 #include <stdio.h>
 #include <string.h>
 #define MAXCHR 1000
-
+typedef int bool;
+#define true 1
+#define false 0
 /*
     written by Taner Durmaz
-    This program is representation of DFA which accepts only even number of 1's
-    input sequence must consist of 1's and 0's 
+    contact: taner.durmaz.cs@gmail.com
 */
 
-int main(int argc, char **argv){
-    enum STATES {A, B} state;
+//Accepts only even number of 1's
+bool even1s(){
     char w[MAXCHR];
-    int i;
-
-    if (argc != 2) {
-        printf("%s <w as binary string>\n", argv[0]);
-        return -1;
-    }
-  
-    strcpy(w, argv[1]);
-  
+    enum STATES {A, B} state;
+    printf("Enter the string \n");
+    scanf("%s", w);
+    
     // starting state is A
     state = A;
-    
-    for (i=0; i<strlen(w); i++){
+  
+    for (int i = 0; i<strlen(w); i++){
         switch(state){
             case A:
-                if (w[i]== '0') {
-                    state = A;
+                if (w[i]== '1') {
+                    state = B;
                 }
                 else{
-                    state = B;
+                    state = A;
                 }
                 break;
             case B:
-                if (w[i]== '0') {
-                    state = B;
+                if (w[i]== '1') {
+                    state = A;
                 }
                 else{
-                    state = A;
+                    state = B;
                 }
                 break;
         }
     }
 
-    if (state == A)
+    if (state == A){
         printf("accepted, sequence %s has even number of 1's \n", w);
-    else
+        return true;
+    }
+    else{
         printf("rejected, sequence %s has odd number of 1's  \n", w);
+        return false;
+    }
 }
+
+int main(int argc){
+    int dfaNo;
+    do{
+        printf("Available DFA\n");
+        printf("0 Exit\n");
+        printf("1 Accept even number of 1's\n");
+        printf("----------------------------\n");
+        printf("Choose DFA\n");
+        scanf("%d", &dfaNo);
+        if(dfaNo == 1){
+            even1s();
+        }
+        else{
+            printf("Invalid input try again\n");
+        }
+    }while(dfaNo != 0);
+    
+    return 0;
+}
+
